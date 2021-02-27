ffa436eda9484e60462d4712430d75badfe378eb
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-27 12:57:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-starter-alipay4j
+ 
---
+++ src/test/java/cn/org/alipay4j/boot/Alipay4JApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.alipay4j.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alipay4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
